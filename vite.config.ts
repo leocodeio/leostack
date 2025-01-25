e4f4f72d3896fb80a0ef377700f8ee1eb948b006
leocodeio <basicleo787@gmail.com>
2025-01-25 05:37:28 +0000

    commit before sesssion imp

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
+import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   plugins: [
+    remixCloudflareDevProxy(),
     remix({
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes);
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
